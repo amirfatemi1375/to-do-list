2e27dcca8610692f88f844294d313f58e18ec613
amir fatemi <amirhosseinfatemi559@gmail.com>
2023-12-24 18:01:31 +0330

    [feat] custom layout modules

---
+++ src/app/layout/custom-layout/custom-layout.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../../shared/shared.module';
+import { CustomLayoutComponent } from './custom-layout.component';
+
+@NgModule({
+  declarations: [CustomLayoutComponent],
+  imports: [CommonModule, SharedModule],
+})
+export class CustomLayoutModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MaterialModule } from './modules/material.module';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NavComponent } from './components/nav/nav.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [HeaderComponent, FooterComponent, NavComponent],
   imports: [CommonModule, MaterialModule],
+  exports: [HeaderComponent, FooterComponent, NavComponent],
 })
 export class SharedModule {}
